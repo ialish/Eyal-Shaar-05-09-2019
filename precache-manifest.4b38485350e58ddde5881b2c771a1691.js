18df5bfa7442816f82102740b5f1a53ce845309b
ialish <eyal.shaar@gmail.com>
2019-09-18 06:16:49 +0300

    Updates

---
+++ precache-manifest.4b38485350e58ddde5881b2c771a1691.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982b9b342711b96297e83cdee3f62587",
+    "url": "/Eyal-Shaar-05-09-2019/index.html"
+  },
+  {
+    "revision": "a7274c8b47d969ac7eb5",
+    "url": "/Eyal-Shaar-05-09-2019/static/css/main.9b245654.chunk.css"
+  },
+  {
+    "revision": "5b93d0c969a9320cbb79",
+    "url": "/Eyal-Shaar-05-09-2019/static/js/2.26a0f8d3.chunk.js"
+  },
+  {
+    "revision": "a7274c8b47d969ac7eb5",
+    "url": "/Eyal-Shaar-05-09-2019/static/js/main.96354396.chunk.js"
+  },
+  {
+    "revision": "91a27a8782e4bff2d63d",
+    "url": "/Eyal-Shaar-05-09-2019/static/js/runtime~main.ec445260.js"
+  },
+  {
+    "revision": "efb08012749ca20ff3223cc32acdb2ac",
+    "url": "/Eyal-Shaar-05-09-2019/static/media/HeartFull.efb08012.svg"
+  },
+  {
+    "revision": "4ed658431d929c20f41f3e7606723cf3",
+    "url": "/Eyal-Shaar-05-09-2019/static/media/HeartHollow.4ed65843.svg"
+  },
+  {
+    "revision": "e108e41599c27c225b8fc82ce864909d",
+    "url": "/Eyal-Shaar-05-09-2019/static/media/bg.e108e415.jpg"
+  }
+]);
