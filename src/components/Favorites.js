40cb6748d1e40895d2b451dc396aa53172337fb6
ialish <eyal.shaar@gmail.com>
2019-09-26 12:05:43 +0300

    Update

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -14,32 +14,36 @@ class Favorites extends Component {
 	componentDidMount() {
 		this.setState({ favCities: JSON.parse(localStorage.getItem('Favorite Cities')) });
 	}
+
+	componentDidUpdate() {
+		this.state.favCities.map((location) => {
+			const url = `https://dataservice.accuweather.com/currentconditions/v1/${location.key}?apikey=${apiKey}`;
+			let degreesC = null;
+			let weatherText = '';
+			fetch(url)
+				.then(resp => resp.json())
+				.then(json => {
+					degreesC = Math.round(json[0].Temperature.Metric.Value);
+					weatherText = json[0].WeatherText;
+				});
+			return (
+				<Card key={location.key} style={{ width: '18rem' }}>
+					<Card.Body>
+						<Card.Title>{location.city}</Card.Title>
+						<Card.Subtitle className="mb-2">
+							{degreesC}
+							{weatherText}
+						</Card.Subtitle>
+					</Card.Body>
+				</Card>
+			);
+		})
+	}
 	
 	render() {
 		return (
 			<div style={{ display: 'flex', flexDirection: 'row', marginTop: '2rem' }}>
-				{this.state.favCities.map((location) => {
-					const url = `https://dataservice.accuweather.com/currentconditions/v1/${location.key}?apikey=${apiKey}`;
-					let degreesC = null;
-					let weatherText = '';
-					fetch(url)
-						.then(resp => resp.json())
-						.then(json => {
-							degreesC = Math.round(json[0].Temperature.Metric.Value);
-							weatherText = json[0].WeatherText;
-						});
-					return (
-						<Card key={location.key} style={{ width: '18rem' }}>
-							<Card.Body>
-								<Card.Title>{location.city}</Card.Title>
-								<Card.Subtitle className="mb-2">
-									{degreesC}
-									{weatherText}
-								</Card.Subtitle>
-							</Card.Body>
-						</Card>
-					);
-				})}
+				{}
 			</div>
 		);
 	}
