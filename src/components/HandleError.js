f45028f182f8e402e203db125cafec44b8193708
ialish <eyal.shaar@gmail.com>
2019-09-28 16:58:37 +0300

    Update

--- src/components/HandleError.js
+++ src/components/HandleError.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import { Toast/* , Modal */ } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Toast } from 'react-bootstrap';
 
 const HandleError = ({ name, description }) => {
+	const [show, setShow] = useState(true);
+	const toggleShow = () => setShow(!show);
 	return (
 		<div>
-			<Toast>
+			<Toast show={show} onClose={toggleShow}>
 				<Toast.Header>
-					<img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
 					<strong className="mr-auto">{name}</strong>
 				</Toast.Header>
 				<Toast.Body>{description}</Toast.Body>
 			</Toast>
-			{/* <Modal.Dialog>
-				<Modal.Header>
-					<Modal.Title>{name}</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<p>{description}</p>
-				</Modal.Body>
-			</Modal.Dialog> */}
 		</div>
 	);
 }
