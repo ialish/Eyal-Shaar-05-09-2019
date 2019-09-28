70fa9ddfca2cf04a6657b97ab15a87ff98207c38
ialish <eyal.shaar@gmail.com>
2019-09-28 16:28:54 +0300

    Update

--- src/components/HandleError.js
+++ src/components/HandleError.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Modal } from 'react-bootstrap';
+import { Toast/* , Modal */ } from 'react-bootstrap';
 
 const HandleError = ({ name, description }) => {
 	return (
-		<Modal.Dialog>
-			<Modal.Header>
-				<Modal.Title>{name}</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<p>{description}</p>
-			</Modal.Body>
-		</Modal.Dialog>
+		<div>
+			<Toast>
+				<Toast.Header>
+					<img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+					<strong className="mr-auto">{name}</strong>
+				</Toast.Header>
+				<Toast.Body>{description}</Toast.Body>
+			</Toast>
+			{/* <Modal.Dialog>
+				<Modal.Header>
+					<Modal.Title>{name}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<p>{description}</p>
+				</Modal.Body>
+			</Modal.Dialog> */}
+		</div>
 	);
 }
 
