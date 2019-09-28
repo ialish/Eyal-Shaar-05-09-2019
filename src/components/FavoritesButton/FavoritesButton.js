5ce2e14fcb62a301826cc97a894f464bbd8a0f55
ialish <eyal.shaar@gmail.com>
2019-09-28 14:44:50 +0300

    Update

--- src/components/FavoritesButton/FavoritesButton.js
+++ src/components/FavoritesButton/FavoritesButton.js
@@ -7,8 +7,8 @@ class FavoritesButton extends Component {
 	constructor() {
 		super();
 		this.state = {
-			heartImage: HeartHollow,
-			favCities: []
+			favCities: [],
+			heartImage: HeartHollow
 		};
 	}
 
